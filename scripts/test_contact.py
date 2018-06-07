f8e705a771f960495dfc4e790a9ed02f9db75d43
zengqingmei <1731523541@qq.com>
2018-06-07 11:34:49 +0800

    first commit

---
+++ scripts/test_contact.py
@@ -0,0 +1,17 @@
+from appium import webdriver
+import pytest
+
+
+class TestContact:
+
+    def test_login(self):
+        print("123")
+        assert 1
+
+    def test_login1(self):
+        print("123")
+        assert 1
+
+    def test_login2(self):
+        print("123")
+        assert 0
